291791254411caa4f937e897e03ff111b441dad3
Jean <nguyen.jean@me.com>
2020-09-17 18:32:02 +0200

    add cross svg icon to the nav in responsive

--- public/index.js
+++ public/index.js
@@ -1,10 +1,17 @@
+// toggle menu responsive
 const burger = document.querySelector('#burger');
+const burgerOn = document.querySelector('#burger-on');
+const burgerClose = document.querySelector('#burger-close');
 const menu = document.querySelector('#menu');
 
 burger.addEventListener('click', () => {
   if (menu.classList.contains('hidden')) {
     menu.classList.remove('hidden');
+    burgerClose.classList.remove('hidden');
+    burgerOn.classList.add('hidden');
   } else {
     menu.classList.add('hidden');
+    burgerOn.classList.remove('hidden');
+    burgerClose.classList.add('hidden');
   }
 });
